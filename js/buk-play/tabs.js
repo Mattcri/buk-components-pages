c99549330dbc7ea00cb4bd21cb123279398c2546
Matias-15 <mcriado.cg@gmail.com>
2022-07-12 02:01:57 -0400

    Site: Bukplay - Features: Desktop layout version

--- js/buk-play/tabs.js
+++ js/buk-play/tabs.js
@@ -2,17 +2,30 @@ const btnCollapse = document.getElementById('btn-collapse')
 const menuOptions = document.getElementById('menu-options')
 const arrowCategories = document.querySelector('#btn-collapse .categories-button__arrow')
 const getButtonsNodeList = document.querySelectorAll('#menu-options li button')
+// const getButtonsDeskNodeList = document.querySelectorAll('#menu-options-desk li button')
+const getCategoriesItemsNodeList = document.querySelectorAll('#menu-options .categories-menu__item')
+// const getCategoriesDeskItemsNodeList = document.querySelectorAll('#menu-options-desk .categories-menu__item')
 const dataCategoriesTargetNodeList = document.querySelectorAll('[data-categories-target]')
 const dataCategoriesContentNodeList = document.querySelectorAll('[data-categories-content]')
 
 
 const buttonsToArray = [...getButtonsNodeList]
+// const buttonsDeskToArray = [...getButtonsDeskNodeList]
 const dataTargets = [...dataCategoriesTargetNodeList]
 const dataContent = [...dataCategoriesContentNodeList]
-const categoriesItems = Array.from(document.getElementsByClassName('categories-menu__item'))
+const categoriesItems = [...getCategoriesItemsNodeList]
+// const categoriesDeskItems = [...getCategoriesDeskItemsNodeList]
 const heightMenuOptions = menuOptions.scrollHeight
+const widthOfViewport = window.innerWidth
 
+// window.onresize = resize
 
+// function resize() {
+//   console.log("width: ", window.innerWidth, "px");
+//   if (widthOfViewport >= 1020) {
+//     menuOptions.style.height = `${heightMenuOptions}px`
+//   }
+// }
 
 btnCollapse.addEventListener('click', () => {
   menuOptions.classList.toggle('show')
@@ -33,13 +46,17 @@ const actionsHiddenMenu = () => {
 
 buttonsToArray.forEach(button => {
   button.addEventListener('click', function () {
-    menuOptions.classList.remove('show')
-    menuOptions.style.height = "0px"
-    arrowCategories.classList.remove('rotate')
 
+    // let widthOfViewport = window.innerWidth
     let getTargetId = this.dataset.categoriesTarget.slice(1)
     let target = document.getElementById(getTargetId)
     let getButtonText = this.children[1].textContent
+
+    if (widthOfViewport < 1020) {
+      menuOptions.classList.remove('show')
+      menuOptions.style.height = "0px"
+      arrowCategories.classList.remove('rotate')
+    }
     
     dataContent.forEach(content => content.classList.remove('target-active'))
 
@@ -51,13 +68,31 @@ buttonsToArray.forEach(button => {
     console.log(getTargetId);
     console.log(target);
     console.log(getButtonText);
+    console.log(widthOfViewport);
   })
 })
+
+// buttonsDeskToArray.forEach(button => {
+//   button.addEventListener('click', function() {
+//     let getTargetId = this.dataset.categoriesTarget.slice(1)
+//     let target = document.getElementById(getTargetId)
+
+//     dataContent.forEach(content => content.classList.remove('target-active'))
+
+//     buttonsDeskToArray.forEach(item => item.parentElement.classList.remove('target-selected'))
+
+//     target.classList.add('target-active')
+//     button.parentElement.classList.add('target-selected')
+//   })
+// })
 
 // Show the first content
 dataContent[0].classList.add('target-active')
 categoriesItems[0].classList.add('target-selected')
+// categoriesDeskItems[0].classList.add('target-selected')
+
 
 
 
 console.log(getButtonsNodeList);
+console.log(categoriesItems)
