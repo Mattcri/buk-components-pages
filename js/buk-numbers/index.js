80800d565ba0596c56fc1bd75bbf5784719b0215
Matias-15 <mcriado.cg@gmail.com>
2022-10-27 02:38:36 -0300

    Page: buk-numbers - Feature: use data buk-numbers for building graphics and convert seconds to formatt value

--- js/buk-numbers/index.js
+++ js/buk-numbers/index.js
@@ -1,30 +1,67 @@
-const API = "https://mattguz.github.io/buk-numbers/graphics-api.json"
+const API = "https://mattguz.github.io/buk-numbers/data_buknumbers.json"
 
 
 const getData = async (urlapi) => {
-  let response = await fetch(urlapi)
-  let data = await response.json()
-  return data
+  let response = await fetch(urlapi) 
+  return response.json()
 }
 
 const getChat = async () => {
   try {
-    let chat = await getData(API).then(response => response.chat)
+    let chat = await getData(API).then(response => response.chat.responseTime)
     let dates = chat.data.map(e => e.date)
-    let avgs = chat.data.map(e => e.averagePerDay)
+    let avgs = chat.data.map(e => e.avg_responseTime_chat)
     let avg = chat.average90Days
-    let minute = Math.floor(avg).toString()
-    let seconds = avg.toFixed(2).toString().slice(2)
-    console.log(dates)
-    console.log(typeof avg, avg)
-    console.log('minutos: ', typeof minute, minute)
-    console.log('segundos: ', typeof seconds, seconds)
-    const DOMavg = document.getElementById('chatAvg')
+    let valuesMin = avgs.map(e => Math.floor(e / 60).toString())
+    let valuesSec = avgs.map(e => {
+      let second = Math.floor(e % 60)
+      if (second >= 10) {
+        return second.toString()
+      } else {
+        return second.toString().padStart(2, '0')
+      }
+    })
+
+    let minutesAndsecond = []
+    
+    for (let i = 0; i < valuesMin.length; i++ ) {
+      let min = valuesMin[i]
+      let sec = valuesSec[i]
+      let formatt = `${min}.${sec}`
+      minutesAndsecond.push(formatt)
+    }
+    // let t = valuesMin.join(`.${valuesSec}`)
+
+    console.log('Min:', valuesMin)
+    console.log('Sec: ', valuesSec)
+    console.log('A VERR', minutesAndsecond)
+
+
+    // let minute = Math.floor(avg).toString()
+    // let seconds = avg.toFixed(2).toString().slice(2)
+
+    // let i = 1
+    // avgs.forEach(item => {
+    //   let fomattMinutes = Math.trunc(item / 60)
+    //   let formattSeconds = item % 60
+    
+    //   console.log(i++, '-' , fomattMinutes)
+    //   console.log(formattSeconds)
+    // })
+
+    console.log('Promedio chat: ', avg)
+    console.log('Fechas chat: ', dates)
+    console.log('Promedios chat: ', avgs)
+    
+    // console.log(typeof avg, avg)
+    // console.log('minutos: ', typeof minute, minute)
+    // console.log('segundos: ', typeof seconds, seconds)
+    // const DOMavg = document.getElementById('chatAvg')
     const ctx = document.getElementById('graphiChat').getContext('2d')
     
-    DOMavg.textContent = `${minute} min ${seconds} seg`
-    const chart = new Chart(ctx, abstractConfig(dates, avgs, 'Minutos'))
-
+    // DOMavg.textContent = `${minute} min ${seconds} seg`
+    const chart = new Chart(ctx, abstractConfig(dates.reverse(), minutesAndsecond.reverse(), 'Minutos'))
+    
   } catch (err) {
     console.error(err)
   }
@@ -32,20 +69,22 @@ const getChat = async () => {
 
 const getCall = async () => {
   try {
-    let chat = await getData(API).then(response => response.call)
-    let dates = chat.data.map(e => e.date)
-    let avgs = chat.data.map(e => e.averagePerDay)
-    let avg = chat.average90Days
+    let call = await getData(API).then(response => response.call.responseTime)
+    let dates = call.data.map(e => e.date)
+    let avgs = call.data.map(e => e.avg_answerSpeed_call)
+    let avg = call.average90Days
     let minute = Math.floor(avg)
     let seconds = avg.toString().slice(2)
-    console.log(dates)
-    console.log(typeof avg, avg)
-    console.log('minutos call: ', typeof minute, minute)
-    console.log('segundos call: ', typeof seconds, seconds)
-    const DOMavg = document.getElementById('callAvg')
+    console.log('Promedio call: ', avg)
+    console.log('Fechas call: ', dates)
+    console.log('Promedios call: ', avgs)
+    // console.log(typeof avg, avg)
+    // console.log('minutos call: ', typeof minute, minute)
+    // console.log('segundos call: ', typeof seconds, seconds)
+    // const DOMavg = document.getElementById('callAvg')
     const ctx = document.getElementById('graphiCall').getContext('2d')
 
-    DOMavg.textContent = `${minute} min ${seconds} seg`
+    // DOMavg.textContent = `${minute} min ${seconds} seg`
     const chart = new Chart(ctx, abstractConfig(dates, avgs, 'Minutos'))
 
   } catch (err) {
@@ -55,20 +94,22 @@ const getCall = async () => {
 
 const getEmail = async () => {
   try {
-    let chat = await getData(API).then(response => response.email)
-    let dates = chat.data.map(e => e.date)
-    let avgs = chat.data.map(e => e.averagePerDay)
-    let avg = chat.average90Days
+    let email = await getData(API).then(response => response.email.responseTime)
+    let dates = email.data.map(e => e.date)
+    let avgs = email.data.map(e => e.avg_responseTime_email)
+    let avg = email.average90Days
     let hours = Math.floor(avg).toString()
     let minutes = avg.toFixed(2).toString().slice(2)
-    console.log(dates)
-    console.log(typeof avg, avg)
-    console.log('minutos email: ', typeof hours, hours)
-    console.log('segundos email: ', typeof minutes, minutes)
-    const DOMavg = document.getElementById('emailAvg')
+    console.log('Promedio email: ', avg)
+    console.log('Fechas email: ', dates)
+    console.log('Promedios email: ', avgs)
+    // console.log(typeof avg, avg)
+    // console.log('minutos email: ', typeof hours, hours)
+    // console.log('segundos email: ', typeof minutes, minutes)
+    // const DOMavg = document.getElementById('emailAvg')
     const ctx = document.getElementById('graphicEmail').getContext('2d')
 
-    DOMavg.textContent = `${hours} h ${minutes} min`
+    // DOMavg.textContent = `${hours} h ${minutes} min`
     const chart = new Chart(ctx, abstractConfig(dates, avgs, 'Horas'))
 
   } catch (err) {
@@ -103,15 +144,24 @@ const abstractConfig = (date, avg, time) => {
               weight: 'bold',
             }
           },
+          // stackWeight: .60,
           min: 0,
           max: 4,
-          // suggestedMin: 10,
-          // suggestedMax: 100
+          // suggestedMax: 60,
+          // suggestedMin: 0,
+          // suggestedMax: 60,
           ticks: {
             stepSize: 1,
+            // crossAlign: 'center',
+            backdropPadding: 2,
             padding: 5
           },
           
+        },
+        x: {
+          ticks: {
+            maxTicksLimit: 7,
+          }
         }
       },
       
