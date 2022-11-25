56a0729d88deb06761a29d200f700f1da3849102
Matias-15 <mcriado.cg@gmail.com>
2022-11-25 20:57:02 -0300

    Page: perks-calculator - Feature: add tooltips, config new values and click free benefits

--- js/soluciones/beneficios-calc.js
+++ js/soluciones/beneficios-calc.js
@@ -1,4 +1,6 @@
 // let total = 0
+moment.locale('es')
+
 const getUF = async () => {
   const response = await fetch('https://d3nk1otf0qe6jp.cloudfront.net/data.json')
     .then(r => r.json())
@@ -11,10 +13,14 @@ const getUF = async () => {
 
 const putUF = async () => {
   const call = await getUF()
+  const DOMufDate = document.getElementById('date-uf')
   const uf = call.Valor
   const date = call.Fecha
+  const dateFormat = moment(date).format('D MMM YYYY')
+  const ufvalueClp = parseFloat(uf)
   valuesUF.price = uf
   valuesUF.date = date
+  DOMufDate.textContent = `$${ufvalueClp} - ${dateFormat}`
 }
 
 const valuesUF = {
@@ -47,11 +53,12 @@ const calculateValue = (checkbox) => {
   const actives = checkbox.filter(c => c.checked)
   const values = actives.map(v => parseFloat(v.value))
   const total = values.reduce((prev, acum) => prev + acum, 0)
-  putPricesInDOM(total.toFixed(2))
-  console.log(checkbox)
-  console.log(actives)
-  console.log(values)
-  console.log(total.toFixed(2))
+  putPricesInDOM(total.toFixed(3))
+  // console.log(checkbox)
+  // console.log(actives)
+  console.log('Suma de valores: ', values)
+  console.log('Total: ', total)
+  console.log('-----------------')
 }
 
 const putPricesInDOM = (price) => {
@@ -70,8 +77,8 @@ const putPricesInDOM = (price) => {
   })
   DOMclp.textContent = Math.floor(ufToClp) == 0 ? `Aprox. $${clp.slice(3)} CLP` : `Aprox. ${clp} CLP`
   
-  console.log(parseFloat(ufPrice))
-  console.log(ufToClp)
+  console.log('UfPrice: ', parseFloat(ufPrice))
+  console.log('toClp', ufToClp)
   console.log('CLP: ',clp)
 }
 
@@ -80,7 +87,7 @@ const showError = () => {
   const gympassStarter = document.getElementById('bn-4')
   const DOMuf = document.getElementById('price-uf')
   const DOMclp = document.getElementById('price-clp')
-  DOMuf.textContent = 'Selecciona solo un Gympass'
+  DOMuf.textContent = 'Selecciona solo una versión de Gympass'
   DOMclp.textContent = 'Aprox $0 CLP'
   DOMuf.classList.add('txt-error')
   gympassCore.parentElement.classList.add('control-error')
@@ -96,7 +103,14 @@ const hideError = () => {
   gympassStarter.parentElement.classList.remove('control-error')
 }
 
+const initialClick = async () => {
+  document.getElementById('bn-2').click()
+}
+
 
 if (typeof window !== "undefined") {
-  window.onload = putUF()
+  window.onload = async () => {
+    await putUF()
+    await initialClick()
+  }
 }
