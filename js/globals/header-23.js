868379d82da8b38d60751cb29dd979821f440dcb
Matias-15 <mcriado.cg@gmail.com>
2023-05-03 17:09:46 -0400

    Page: null - Feature: create styles and functionalities for collapse menu mobile

--- js/globals/header-23.js
+++ js/globals/header-23.js
@@ -1,5 +1,8 @@
 const btnCountries = [...document.querySelectorAll('.buk-countries')]
-const btnMobile = document.getElementById('btn-mobile')
+const btnCollapse = document.getElementById('btn-collapse')
+const btnDropdown = [...document.querySelectorAll('.nav__item button')]
+
+const mql = window.matchMedia('(max-width:1020px)')
 
 btnCountries.forEach(btn => {
   btn.addEventListener('click', function () {
@@ -20,6 +23,38 @@ document.addEventListener('scroll', () => {
   }
 })
 
-btnMobile.addEventListener('click', function () {
-  this.classList.toggle('show-menu')
+btnCollapse.addEventListener('click', function () {
+  this.classList.toggle('open-collapse')
+  if (this.classList.contains('open-collapse')) {
+    document.querySelector('body').classList.add('non-scroll')
+  } else {
+    document.querySelector('body').classList.remove('non-scroll')
+  }
+})
+
+btnDropdown.forEach((button, index) => {
+  // console.log(index, item)
+  button.addEventListener('click', function () {
+    let activeButton = document.querySelector('.nav__item button.active')
+    let heightSubmenu = button.nextElementSibling.scrollHeight
+
+    btnDropdown.forEach(btn => btn.nextElementSibling.style.height = '0px')
+    
+    if (activeButton && activeButton !== button) {
+      activeButton.classList.remove('active')
+      activeButton.nextElementSibling.classList.remove('open')
+      // activeButton.nextElementSibling.style.height = '0px'
+      activeButton.parentElement.classList.remove('item-selected')
+    }
+
+    this.classList.toggle('active')
+    this.nextElementSibling.classList.toggle('open')
+    this.parentElement.classList.toggle('item-selected')
+    
+    if (button.classList.contains('active')) {
+      this.nextElementSibling.style.height = `${heightSubmenu}px`
+    }
+
+
+  })
 })
