981eef4825ce27a72b786e33d3d5c95c9d5e53d0
Matias-15 <mcriado.cg@gmail.com>
2023-10-16 23:19:40 -0300

    Page: calc-aguinald-mx - Feature: changes in frontend, add new fields discharge date option and type tax

--- js/calcs/calc-aguinaldo-mx.js
+++ js/calcs/calc-aguinaldo-mx.js
@@ -3,6 +3,20 @@ import { Director } from "./modules/aguinaldo-mx/director.js"
 const director = new Director()
 
 const lisrDetail = document.getElementById('lisr-detail')
+const dischargeDateOptions = [...document.getElementsByName('discharge-date-option')]
+
+dischargeDateOptions.forEach(option => {
+  option.addEventListener('change', function () {
+    let dischargeDateVisibility = document.getElementById('discharge-date-visibility')
+    if (option.id == "bol-discharge-date-true") {
+      dischargeDateVisibility.classList.remove('ds-none')
+    } else {
+      let dsDate = document.getElementById('discharge-date')
+      dischargeDateVisibility.classList.add('ds-none')
+      dsDate.value = ''
+    }
+  })
+})
 
 lisrDetail.addEventListener('click', function() {
   let dropdown = document.querySelector('.calc__fields.calc__fields--dropdown')
