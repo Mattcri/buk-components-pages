e543cdb01a412b430bc020b71c501537c06dab55
Matias-15 <mcriado.cg@gmail.com>
2024-02-15 02:47:33 -0300

    Page: previred-cl - Feature: create vue app, fetch data and components

---
+++ js/vue/components/pr-tables.js
@@ -0,0 +1,9 @@
+export default {
+  data() {
+    return { count: 0 }
+  },
+  template: `
+  <div class="txt-center"><button @click="count++" class="btn btn--2">Sumar</button></div>
+  <div class="buk-mt-3">count is {{ count }}</div>
+  `
+}
