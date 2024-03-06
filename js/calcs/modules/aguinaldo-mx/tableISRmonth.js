b353dcddaa239070acb2d060920fdcb9f3ec9cda
Matias-15 <mcriado.cg@gmail.com>
2024-03-06 15:32:37 -0300

    Page: calc-aguinald-mx - Feature: update UMA and ISR table with values of 2024

--- js/calcs/modules/aguinaldo-mx/tableISRmonth.js
+++ js/calcs/modules/aguinaldo-mx/tableISRmonth.js
@@ -5,16 +5,16 @@ class TableBuilder {
 }
 
 const ISRdataMonth = new TableBuilder([
-  { lowerLimit: 0.01, upperLimit: 644.58, fixedFee: 0, overExc: 1.92},
-  { lowerLimit: 644.59, upperLimit: 5470.92, fixedFee: 12.38, overExc: 6.40},
-  { lowerLimit: 5470.93, upperLimit: 9614.66, fixedFee: 321.26, overExc: 10.88},
-  { lowerLimit: 9614.67, upperLimit: 11176.62, fixedFee: 772.1, overExc: 16},
-  { lowerLimit: 11176.63, upperLimit: 13381.47, fixedFee: 1022.01, overExc: 17.92},
-  { lowerLimit: 13381.48, upperLimit: 26988.50, fixedFee: 1417.12, overExc: 21.36},
-  { lowerLimit: 26988.51, upperLimit: 42537.58, fixedFee: 4323.58, overExc: 23.52},
-  { lowerLimit: 42537.59, upperLimit: 81211.25, fixedFee: 7980.73, overExc: 30},
-  { lowerLimit: 81211.26, upperLimit: 108281.67, fixedFee: 19582.83, overExc: 32},
-  { lowerLimit: 108281.68, upperLimit: 324845.01, fixedFee: 28245.36, overExc: 34}
+  { lowerLimit: 0.01, upperLimit: 746.04, fixedFee: 0, overExc: 1.92 },
+  { lowerLimit: 746.05, upperLimit: 6332.05, fixedFee: 14.32, overExc: 6.40 },
+  { lowerLimit: 6332.06, upperLimit: 11128.01, fixedFee: 371.83, overExc: 10.88 },
+  { lowerLimit: 11128.02, upperLimit: 12935.82, fixedFee: 893.63, overExc: 16 },
+  { lowerLimit: 12935.83, upperLimit: 15487.71, fixedFee: 1182.88, overExc: 17.92 },
+  { lowerLimit: 15487.72, upperLimit: 31236.49, fixedFee: 1640.18, overExc: 21.36 },
+  { lowerLimit: 31236.50, upperLimit: 49233.00, fixedFee: 5004.12, overExc: 23.52 },
+  { lowerLimit: 49233.01, upperLimit: 93993.90, fixedFee: 9236.89, overExc: 30 },
+  { lowerLimit: 93993.91, upperLimit: 125325.20, fixedFee: 22665.17, overExc: 32 },
+  { lowerLimit: 125325.21, upperLimit: 375975.61, fixedFee: 32691.18, overExc: 34 }
 ])
 
 export { ISRdataMonth }
